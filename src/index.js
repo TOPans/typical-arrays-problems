09e64a3eaf2c3a73e9663399f941e89e4419d5fd
TOPans <TOPans@mail.ru>
2021-07-08 15:10:57 +0300

    feat: implement functions min max avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
+exports.min = function min(array) {
+    if (Array.isArray(array) && array.length) {
+        return Math.min(...array);
+    } else {
+        return 0;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (Array.isArray(array) && array.length) {
+        return Math.max(...array);
+    } else {
+        return 0;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (Array.isArray(array) && array.length) {
+        let average = 0;
+        for (let i = 0; i < array.length; i++) {
+            average += array[i];
+        }
+        return average / array.length;
+    } else {
+        return 0;
+    }
+};
